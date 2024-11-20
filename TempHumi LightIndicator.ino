76da14d280756beff5b864892b70d6e3b8be94db
rommel <119675742+rumelito@users.noreply.github.com>
2024-11-20 06:11:38 -0800

    Update TempHumi LightIndicator.ino

--- TempHumi LightIndicator.ino
+++ TempHumi LightIndicator.ino
@@ -1,51 +1,75 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <DHT.h>
-#define DHT_PIN 4
-#define DHT_TYPE DHT22
-#define tempred 34
-#define humidgreen 35
+#include <DHT.h>  
 
+#define DHT_PIN 2
+#define DHT_TYPE DHT11
 
-DHT dht (DHT_PIN, DHT_TYPE);
-Adafruit_SSD1306 display(128,64,&Wire,-1)
+#define GREEN 18 // Green LED pin for humidity
+#define RED 19   // Red LED pin for temperature
+
+DHT dht(DHT_PIN, DHT_TYPE);
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
 void setup() {
-  
   Serial.begin(9600);
   dht.begin();
   delay(2000);
-  if(!display.begin(SSD1306_SWITCHCAPVCC)){
-    Serial.println("CANT READ DETAILS");
+
+  // Initialize the OLED display
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) {
+    Serial.println("Can't initialize display!");
   }
- 
+
+  // Set LED pins as output
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  
+  // Turn off LEDs initially
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
 }
 
 void loop() {
- float humid = dht.readHumidity();
- float temp = dht.readTempreture();
+  float humid = dht.readHumidity();
+  float tempre = dht.readTemperature();
 
- Serial.println("HUMINIDITY");
- Serial.println(humid);
- Serial.println("TEMPRETURE");
- Serial.println( temp);
- delay(1000);
+  // Print/Display sensor values to the Serial Monitor
+  Serial.print("HUMIDITY: ");
+  Serial.println(humid);
+  Serial.print("TEMPERATURE: ");
+  Serial.println(tempre);
+  
+  // Control LEDs based on humidity and temperature
+  if (humid > 70) {
+    digitalWrite(GREEN, HIGH); // Turn on green LED
+    delay(250); // Blink delay
+    digitalWrite(GREEN, LOW);  // Turn off green LED
+    delay(250);
+  } else {
+    digitalWrite(GREEN, LOW);  // Ensure green LED is off
+  }
 
-if(temp >= 30){
-  digitalWrite;(tempred,HIGH)
+  if (tempre > 30) {
+    digitalWrite(RED, HIGH);   // Turn on red LED
+    delay(250); 
+    digitalWrite(RED, LOW);    // Turn off red LED
+    delay(250); 
+  } else {
+    digitalWrite(RED, LOW);    // Ensure red LED is off
+  }
 
-}else{
-   digitalWrite;(tempred,LOW)
+  // Display humidity and temperature on OLED
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 20);
+  display.setTextColor(WHITE);
 
-}
-if(humid <= 50){
-  digitalWrite;(humidgreen,HIGH)
+  display.println("Humidity: " + String(humid) + "%");
+  display.println("Temperature: " + String(tempre) + "C");
+  display.display();
 
-}else{
-   digitalWrite;(humidgreen,LOW)
-
-}
-
-
-
+  // Delay between readings
+  delay(500);
 }
